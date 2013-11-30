716362c4fee09504ece585dd30ebe4981ccf324d
Igor <igor.malkov82@gmail.com>
2013-11-30 12:16:11 +0200

    constructor arr_strata created localy, remove unify

--- corelog.py
+++ corelog.py
@@ -1,15 +1,14 @@
 class CoreLog:
 
+    arr_strata = []
     #constructor
-    def __init__(self, lat=None, lon=None, arr_strata=None):
+    def __init__(self, cl_tag=None, lat=None, lon=None):
        pass
 
     #methods
-    def insert_strata(_strata_object_):
+    def insert_strata(_strata_tag_):
         pass
     def remove_strata(_strata_tag_):
         pass
     def map_strata_arr(foo):
-        pass
-    def unite_strata(s1_tag, s2_taf):
         pass
